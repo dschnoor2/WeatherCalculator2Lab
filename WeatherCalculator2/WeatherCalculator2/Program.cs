0b21bdd18fc1d661bcd9dd468d2c6792f6ab8bfd
dschnoor2 <59981825+dschnoor2@users.noreply.github.com>
2020-01-30 16:46:09 -0600

    If Else Equations
    Updated If/Else equations

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -6,32 +6,47 @@ namespace WeatherCalculator2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("### Weather Calculator 2");
-
-            int temperature;
+            Console.WriteLine("Heat Index or Wind Chill Calculator");
+            double relativeHumidity;
+            double heatIndex;
+            double temperature;
             int windSpeed;
             double windChill;
             string input = "";
 
             // Get the temperature from the user
-            Console.WriteLine("Enter the temperature");
+            Console.WriteLine("Enter the Temperature");
 
             input = Console.ReadLine();
 
             temperature = int.Parse(input);
+            bool istemp = double.TryParse(input, out temperature);
 
-            // Get the windspeed from the user
-            Console.WriteLine("Enter the wind speed");
+            if (temperature > 50)
+            {
+                relativeHumidity = int.Parse(input);
+                Console.WriteLine("Enter Relative Humidity");
+                input = Console.ReadLine();
+                heatIndex = .5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) + (relativeHumidity * 0.094));
 
-            input = Console.ReadLine();
+                Console.WriteLine("The Heat Index is " + heatIndex);
 
-            windSpeed = int.Parse(input);
+            }
+            else if (temperature <= 50)
+            {
+                // Get the windspeed from the user
+                Console.WriteLine("Enter the wind speed in MPH");
 
-            // Calculate the wind chill
+                input = Console.ReadLine();
 
-            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+                windSpeed = int.Parse(input);
 
-            Console.WriteLine("The wind chill is " + windChill);
+                // Calculate the wind chill
+
+                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+
+                Console.WriteLine("The wind chill is " + windChill);
+            }
         }
     }
 }
